9edfcd5f0050913babdc5489107a2d52d9c1299e
radiant1010 <doomsday123@naver.com>
2023-01-19 14:08:34 +0900

    Refresh token 완료

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -14,15 +14,23 @@ export class AuthController {
     return req.user;
   }
   //Refresh Token 재발급
-  @Public()
   @UseGuards(RefreshAuthGuard)
   @Get('refresh-token')
-  reGenRefreshToken(@Res({ passthrough: true }) res, @Request() req) {
+  async reGenRefreshToken(@Res({ passthrough: true }) res, @Request() req) {
     //console.log(req.user);
     //req.user ==> Passport 전략에서 나오는 최종 결과물
     //여기선 Refresh Token 시나리오 적은거 그대로 구현하면됨.
     //cookie도 다시 올려주고
-    console.log('user 정보 :', req.user);
+
+    //Passport에서 토큰 검증을 했는데 토큰 검증을 또 할 필요는 있을까?
+    //Access token 발급
+    const reGenAccessToken = await this.authService.genAccessToken(req.user.user_id);
+    //access token cookie 생성
+    res.cookie('Authorization', reGenAccessToken, {
+      httpOnly: true,
+      //30분 뒤 expire
+      maxAge: 1000 * 60 * 30,
+    });
     return req.user;
   }
 }
